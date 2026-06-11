5e41c4d32f589176c033c22bc4f87767b244197a
Szymon Gmyz <129086138+symoon01@users.noreply.github.com>
2026-06-11 11:48:23 +0200

    added best result in menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,9 +7,25 @@ public class MenuManager : MonoBehaviour
 {
     public GameObject mainMenuPanel;
     public GameObject settingsPanel;
+    public GameObject canvasPanel;
 
     public Slider sensSlider;
     public Slider volumeSlider;
+
+    public void Start()
+    {
+        int bestkills = PlayerPrefs.GetInt("kills", 0);
+        int bestlevel = PlayerPrefs.GetInt("level", 0);
+        int bestwave = PlayerPrefs.GetInt("wave", 0);
+        float besttime = PlayerPrefs.GetFloat("time", 0f);
+
+        canvasPanel.transform.Find("Best kills").GetComponent<TMP_Text>().text = "kills: " + bestkills;
+        canvasPanel.transform.Find("Best score").GetComponent<TMP_Text>().text = "xp level: " + bestlevel;
+        canvasPanel.transform.Find("Best wave").GetComponent<TMP_Text>().text = "wave: " + bestwave;
+        int bestminutes = Mathf.FloorToInt(besttime / 60f);
+        int bestseconds = Mathf.FloorToInt(besttime % 60f);
+        canvasPanel.transform.Find("Best time").GetComponent<TMP_Text>().text = "time: " + $"{bestminutes:00}:{bestseconds:00}";
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene("MainScene");
